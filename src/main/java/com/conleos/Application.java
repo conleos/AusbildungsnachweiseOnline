c081f17db49a4ba321a219f37045c2851390e7d7
YanSchw <schoessow.yannick@gmail.com>
2024-03-18 17:17:52 +0100

    Import Sample Project
    Generated using https://start.vaadin.com

---
+++ src/main/java/com/conleos/Application.java
@@ -0,0 +1,25 @@
+package com.conleos;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/inter", version = "4.5.0")
+@Theme(value = "ausbildungsnachweiseonline")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
