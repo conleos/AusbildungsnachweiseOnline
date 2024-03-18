93492a622eb0268bcc088a501d66e732683de471
YanSchw <schoessow.yannick@gmail.com>
2024-03-18 17:23:05 +0100

    Added first unit test

---
+++ src/test/java/com/conleos/AlwaysPassTest.java
@@ -0,0 +1,13 @@
+package com.conleos;
+
+import org.junit.jupiter.api.Test;
+
+class AlwaysPassTest {
+
+	@Test
+	void alwaysPass() {
+		// This Test should never fail !!!
+		// If it does, something went wrong...
+	}
+
+}
