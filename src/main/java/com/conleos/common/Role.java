f126212eca3c286669bdd4e9e22c8f328dd64450
YanSchw <schoessow.yannick@gmail.com>
2024-03-20 09:26:54 +0100

    Added Roles

---
+++ src/main/java/com/conleos/common/Role.java
@@ -0,0 +1,14 @@
+package com.conleos.common;
+
+public enum Role {
+
+    // Has all Rights and can manage everything
+    Admin,
+
+    // Can manage it's own Trainees create and remove Trainee Acounts
+    Instructor,
+
+    // Only able to fill out forms
+    Trainee
+
+}
--- src/main/java/com/conleos/core/Session.java
+++ src/main/java/com/conleos/core/Session.java
@@ -1,5 +1,6 @@
 package com.conleos.core;
 
+import com.conleos.common.Role;
 import com.vaadin.flow.server.VaadinSession;
 
 import java.util.HashMap;
@@ -11,6 +12,8 @@ import java.util.Map;
 public class Session {
 
     private static Map<VaadinSession, Session> sessions = new HashMap<>();
+
+    private Role sessionRole;
 
     /*
     * Used to validate a User login.
@@ -28,18 +31,27 @@ public class Session {
 
         // TODO: Placeholder and testing only !
         if (username.equals("admin") && passwordHash.equals(String.valueOf(new String("1234").hashCode()))) {
-            return createSession(VaadinSession.getCurrent());
+            return createSession(VaadinSession.getCurrent(), Role.Admin);
         }
 
         return null;
     }
 
     /* Returns a new Session, that is mapped to the current VaadinSession. */
-    private static Session createSession(VaadinSession vaadinSession) {
+    private static Session createSession(VaadinSession vaadinSession, Role role) {
         Session session = new Session();
+
+        session.setSessionRole(role);
 
         sessions.put(vaadinSession, session);
         return session;
+    }
+
+    public void setSessionRole(Role role) {
+        sessionRole = role;
+    }
+    public Role getSessionRole() {
+        return sessionRole;
     }
 
 }
