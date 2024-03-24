b5bacd8c715cf2ae01b0aee5a650e1657ecf3646
YanSchw <schoessow.yannick@gmail.com>
2024-03-24 22:08:31 +0100

    Update MainLayout.java

--- src/main/java/com/conleos/views/MainLayout.java
+++ src/main/java/com/conleos/views/MainLayout.java
@@ -80,7 +80,7 @@ public class MainLayout extends AppLayout {
         Div layout = new Div();
         layout.addClassNames(Display.FLEX, AlignItems.CENTER, Padding.Horizontal.LARGE);
 
-        H1 appName = new H1("AusbildungsnachweiseOnline");
+        H1 appName = new H1("Ausbildungsnachweise Online");
         appName.addClassNames(Margin.Vertical.MEDIUM, Margin.End.AUTO, FontSize.LARGE);
         layout.add(appName);
 
