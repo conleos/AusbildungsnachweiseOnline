aeec0655fbdaddcff7b992024c224da4e574218d
YanSchw <schoessow.yannick@gmail.com>
2024-03-25 11:50:12 +0100

    Changed CreateUserDialog

--- src/main/java/com/conleos/views/admin/CreateUserDialog.java
+++ src/main/java/com/conleos/views/admin/CreateUserDialog.java
@@ -7,6 +7,7 @@ import com.conleos.data.service.UserService;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
@@ -20,19 +21,28 @@ public class CreateUserDialog extends Dialog {
         setWidth("45%");
 
         VerticalLayout dialogLayout = new VerticalLayout();
-        dialogLayout.setWidth("45%");
+        dialogLayout.setWidthFull();
         add(dialogLayout);
 
         TextField username = new TextField("Username");
+        username.setWidthFull();
         PasswordField password = new PasswordField("Initial Password");
+        password.setWidthFull();
         TextField firstName = new TextField("First Name");
+        firstName.setWidthFull();
         TextField lastName = new TextField("Last Name");
+        lastName.setWidthFull();
         TextField email = new TextField("Email");
+        email.setWidthFull();
 
-        dialogLayout.add(username, password, firstName, lastName, email);
+        ComboBox<Role> roleSelect = new ComboBox<>("Role");
+        roleSelect.setItems(Role.values());
+        roleSelect.setItemLabelGenerator(Role::toString);
+
+        dialogLayout.add(username, password, firstName, lastName, email, roleSelect);
 
         Button saveButton = new Button("Create", e -> {
-            User user = new User(username.getValue(), PasswordHasher.hash(password.getValue()), Role.Admin, firstName.getValue(), lastName.getValue());
+            User user = new User(username.getValue(), PasswordHasher.hash(password.getValue()), roleSelect.getValue(), firstName.getValue(), lastName.getValue());
             user.setEmail(email.getValue());
             UserService.getInstance().createUser(user);
             close();
