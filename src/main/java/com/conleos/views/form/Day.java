2fafd0dcf48e4181e2a010222f7d57b34984934c
Peter M. Münzer <p.muenzer@ostfalia.de>
2024-03-27 14:43:42 +0100

    day class

---
+++ src/main/java/com/conleos/views/form/Day.java
@@ -0,0 +1,71 @@
+package com.conleos.views.form;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day {
+
+    List<TextArea> descriptions = new ArrayList<>();
+    List<Integer> timeSum = new ArrayList<>();
+    LocalDate date;
+    public Day (int i) {
+        LocalDate beginOfWeek = LocalDate.now().with(DayOfWeek.MONDAY);
+        this.date = beginOfWeek.plusDays(i);
+    }
+
+    public VerticalLayout createFormContentForDay() {
+        String dayLabel = date.getDayOfWeek().getDisplayName(TextStyle.FULL, UI.getCurrent().getLocale());
+
+        VerticalLayout container = new VerticalLayout();
+
+        Button addBtn = new Button("Add", VaadinIcon.PLUS.create());
+        VerticalLayout day = new VerticalLayout();
+        day.setClassName(LumoUtility.Border.ALL);
+        TextField timeSum = new TextField();
+        timeSum.setLabel("Zeit gesamt:");
+
+        addBtn.addClickListener(event -> {
+            Select<KindOfWork> select = new Select<>();
+            select.setLabel("Art");
+            select.setItems(KindOfWork.PracticalWork, KindOfWork.Schooling);
+            select.setValue(KindOfWork.PracticalWork);
+            TextArea time = new TextArea("Zeit");
+            time.setWidth("60px");
+            time.setHeight("60px");
+            time.addValueChangeListener(timeChange -> {
+
+                //timeSum.setValue(String.valueOf(sum));
+            });
+            TextArea area = new TextArea("Beschreibung");
+            descriptions.add(area);
+            Button delBtn = new Button(VaadinIcon.CLOSE.create());
+            delBtn.addClickListener(eventDel -> {
+                Component parent = delBtn.getParent().get();
+                if (parent instanceof HorizontalLayout) {
+                    container.remove(parent);
+                }
+            });
+            container.add(new HorizontalLayout(select, time, area,delBtn));
+        });
+
+        day.add(new Span(dayLabel), container, addBtn, timeSum);
+        return day;
+    }
+
+}
--- src/main/java/com/conleos/views/form/FormView.java
+++ src/main/java/com/conleos/views/form/FormView.java
@@ -50,7 +50,7 @@ import java.util.stream.Collectors;
 @Route(value = "form", layout = MainLayout.class)
 public class FormView extends VerticalLayout {
 
-    List<TextArea> descriptions = new ArrayList<>();
+    ArrayList<Day> days = new ArrayList<>();
     Select<Integer> nr = new Select<>();
 
     public FormView() {
@@ -73,15 +73,17 @@ public class FormView extends VerticalLayout {
         Hr hr = new Hr();
         add(header,nr,hr,h2);
 
-        LocalDate beginOfWeek = LocalDate.now().with(DayOfWeek.MONDAY);
-        for (int i = 0; i < 5; i++) {
-            createFormContentForDay(beginOfWeek.plusDays(i));
 
+        for (int i = 0; i < 5; i++) {
+            Day day = new Day(i);
+            days.add(day);
+            add(day.createFormContentForDay());
         }
 
         Button saveBtn = new Button("Save");
         saveBtn.addClickListener(save -> {
-            for (TextArea description : descriptions) {
+            for (Day day : days) {
+
                 Session session = Session.getSessionFromVaadinSession(VaadinSession.getCurrent());
                 Form form = new Form(session.getUser());
                 FormService.getInstance().saveForm(form);
@@ -90,55 +92,6 @@ public class FormView extends VerticalLayout {
         add(saveBtn);
     }
 
-    private void createFormContentForDay(LocalDate date) {
-        String dayLabel = date.getDayOfWeek().getDisplayName(TextStyle.FULL, UI.getCurrent().getLocale());
 
-        VerticalLayout container = new VerticalLayout();
-
-        Button addBtn = new Button("Add", VaadinIcon.PLUS.create());
-        VerticalLayout day = new VerticalLayout();
-        day.setClassName(LumoUtility.Border.ALL);
-        TextField timeSum = new TextField();
-        timeSum.setLabel("Zeit gesamt:");
-
-        addBtn.addClickListener(event -> {
-            Select<KindOfWork> select = new Select<>();
-            select.setLabel("Art");
-            select.setItems(KindOfWork.PracticalWork, KindOfWork.Schooling);
-            select.setValue(KindOfWork.PracticalWork);
-            TextArea time = new TextArea("Zeit");
-            time.setWidth("60px");
-            time.setHeight("60px");
-            time.addValueChangeListener(timeChange -> {
-                List<Component> components = container.getChildren().toList();
-                double sum = 0;
-                for (Component component : components) {
-                    if (component instanceof TextField && component.getId().isPresent() && component.getId().get().equals("time")) {
-                        try {
-                            Double zeit = Double.parseDouble(((TextField) component).getValue());
-                            System.out.println(zeit);
-                            sum += zeit;
-                        } catch (NumberFormatException e) {
-                            System.out.println("Fehler bei Zeitwert.");
-                        }
-                    }
-                }
-                timeSum.setValue(String.valueOf(sum));
-            });
-            TextArea area = new TextArea("Beschreibung");
-            descriptions.add(area);
-            Button delBtn = new Button(VaadinIcon.CLOSE.create());
-            delBtn.addClickListener(eventDel -> {
-                Component parent = delBtn.getParent().get();
-                if (parent instanceof HorizontalLayout) {
-                    container.remove(parent);
-                }
-            });
-            container.add(new HorizontalLayout(select, time, area,delBtn));
-        });
-
-        day.add(new Span(dayLabel), container, addBtn, timeSum);
-        add(day);
-    }
 
 }
