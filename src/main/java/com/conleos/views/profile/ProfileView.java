2509dd014c33c30571562511b220483f141c1834
Peter M. Münzer <p.muenzer@ostfalia.de>
2024-04-16 22:27:05 +0200

    Update ProfileView.java

--- src/main/java/com/conleos/views/profile/ProfileView.java
+++ src/main/java/com/conleos/views/profile/ProfileView.java
@@ -6,11 +6,15 @@ import com.conleos.data.service.UserService;
 import com.conleos.views.MainLayout;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.VaadinSession;
+
+import java.awt.*;
 
 @PageTitle("Edit your Profile")
 @Route(value = "profile", layout = MainLayout.class)
@@ -33,7 +37,34 @@ public class ProfileView extends VerticalLayout {
         lastName.setValue(user.getLastName());
         lastName.setEnabled(false);
 
-        return new VerticalLayout(firstName, lastName, new Button("Reset Password"));
+        Dialog dialog = new Dialog();
+
+        dialog.setHeaderTitle(
+                String.format("Change Password", user.getFullName()));
+
+        dialog.add("Are you sure you want to change your password?");
+        TextField oldPasswordField = new TextField("Old Password");
+        TextField newPasswordField = new TextField("New Password");
+        VerticalLayout input = new VerticalLayout();
+        input.add(oldPasswordField,newPasswordField);
+        dialog.add(input);
+        Button changeButton = new Button("Change");
+        changeButton.addClickListener(clickEvent -> {
+
+            dialog.close();
+        });
+        changeButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY,
+                ButtonVariant.LUMO_ERROR);
+        changeButton.getStyle().set("margin-right", "auto");
+        dialog.getFooter().add(changeButton);
+
+        Button cancelButton = new Button("Cancel", (e) -> dialog.close());
+        cancelButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        dialog.getFooter().add(cancelButton);
+
+        Button change = new Button("Change Password", e -> dialog.open());
+
+        return new VerticalLayout(firstName, lastName, dialog, change);
     }
 
 }
