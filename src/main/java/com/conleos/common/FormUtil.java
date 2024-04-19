b29647a21dc668bfd7bce2b4ae874fa47448555a
YanSchw <schoessow.yannick@gmail.com>
2024-04-19 23:08:43 +0200

    Create FormUtil.java

---
+++ src/main/java/com/conleos/common/FormUtil.java
@@ -0,0 +1,33 @@
+package com.conleos.common;
+
+import com.conleos.data.entity.Form;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Span;
+
+public class FormUtil {
+    private FormUtil() {
+    }
+
+    public static Span createFormBadge(Form form) {
+        if (form == null || form.getStatus() == null) {
+            return new Span("Create Form");
+        }
+        Span badge = new Span();
+        switch (form.getStatus()) {
+            case InProgress -> badge.getElement().setAttribute("theme", "badge");
+            case InReview -> badge.getElement().setAttribute("theme", "badge contrast");
+            case Signed -> badge.getElement().setAttribute("theme", "badge success");
+            case Rejected -> badge.getElement().setAttribute("theme", "badge error");
+        }
+
+        switch (form.getStatus()) {
+            case InProgress -> badge.setText("In Progress");
+            case InReview -> badge.setText("In Review");
+            case Signed -> badge.setText("Signed");
+            case Rejected -> badge.setText("Rejected");
+        }
+
+        return badge;
+    }
+
+}
