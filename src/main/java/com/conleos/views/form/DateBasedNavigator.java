d72ca6d59daecf8cd44ff7ff835b61e2fa98c914
YanSchw <schoessow.yannick@gmail.com>
2024-04-29 23:35:25 +0200

    Refactored DateBasedNavigator

--- src/main/java/com/conleos/views/form/DateBasedNavigator.java
+++ src/main/java/com/conleos/views/form/DateBasedNavigator.java
@@ -1,17 +1,29 @@
 package com.conleos.views.form;
 
+import com.conleos.data.entity.Form;
 import com.conleos.data.entity.User;
 import com.vaadin.flow.component.datepicker.DatePicker;
 
 import java.time.LocalDate;
 
 /*
-* This DatePicker is used to navigate to specific Form by picking a date.
-* An EventListener will automatically reroute the client.
-* */
+ * This DatePicker is used to navigate to specific Form by picking a date.
+ * An EventListener will automatically reroute the client.
+ * */
 public class DateBasedNavigator extends DatePicker {
 
+
+    public DateBasedNavigator(Form form) {
+        if (form != null) {
+            init(form.getOwner(), form.getMondayDate());
+        }
+    }
+
     public DateBasedNavigator(User trainee, LocalDate date) {
+        init(trainee, date);
+    }
+
+    private void init(User trainee, LocalDate date) {
         setValue(date);
         setLabel("Navigate by Date");
         addValueChangeListener(event -> {
